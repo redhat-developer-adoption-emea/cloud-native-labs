0123a409eb9af87ef0fea4e586cbaa846832e145
user1 <fff@ko.com>
2019-01-19 09:25:22 +0100

    lab-2-dotnet-core adding openapi generator config file to modify package name

--- solutions/lab-2-dotnet-core/generate-code.sh
+++ solutions/lab-2-dotnet-core/generate-code.sh
@@ -3,4 +3,4 @@
 mkdir ./bin
 curl -L -o ./bin/openapi-generator-cli.jar http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/3.3.4/openapi-generator-cli-3.3.4.jar
 
-java -jar ./bin/openapi-generator-cli.jar generate -i inventory.yaml -g aspnetcore -o inventory-gen
+java -jar ./bin/openapi-generator-cli.jar generate -i inventory.yaml -g aspnetcore -o inventory-gen -c openapi-config.json
